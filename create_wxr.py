b59166fbec7570ee5685be7e558ae04f4925a012
Emil Shagiev <lesshishkin@gmail.com>
2024-08-03 19:35:16 +0500

    main, create

--- create_wxr.py
+++ create_wxr.py
@@ -1,9 +1,10 @@
 from lxml import etree
 from lxml.etree import QName
+import os
+from configs import output_dir
 
 
-def create_wxr(posts, output_path):
-    # Создаем корневой элемент
+def create_wxr(posts, output_file_name):
     rss = etree.Element("rss", version="2.0", nsmap={
         None: "http://purl.org/rss/1.0/modules/content/",
         "excerpt": "http://wordpress.org/export/1.2/excerpt/",
@@ -12,26 +13,21 @@ def create_wxr(posts, output_path):
         "wp": "http://wordpress.org/export/1.2/"
     })
 
-    # Создаем канал
     channel = etree.SubElement(rss, "channel")
-    etree.SubElement(channel, "title").text = "Your Blog Title"
-    etree.SubElement(channel, "link").text = "http://yourblog.com"
-    etree.SubElement(channel, "description").text = "Description of your blog"
-    etree.SubElement(channel, "language").text = "en-US"
     etree.SubElement(channel, QName("http://wordpress.org/export/1.2/", "wxr_version")).text = "1.2"
 
-    # Добавляем статьи
     for post in posts:
         item = etree.SubElement(channel, "item")
+
         etree.SubElement(item, "title").text = post["title"]
         etree.SubElement(item, "link").text = post["link"]
         etree.SubElement(item, "pubDate").text = post["pubDate"]
-        etree.SubElement(item, QName("http://purl.org/dc/elements/1.1/", "creator")).text = post["author"]
+        etree.SubElement(item, QName("http://purl.org/dc/elements/1.1/", "creator")).text = post["creator"]
         content = etree.SubElement(item, QName("http://purl.org/rss/1.0/modules/content/", "encoded"))
         content.text = etree.CDATA(post["content"])
         excerpt = etree.SubElement(item, QName("http://wordpress.org/export/1.2/excerpt/", "encoded"))
         excerpt.text = etree.CDATA(post.get("excerpt", ""))
-        etree.SubElement(item, QName("http://wordpress.org/export/1.2/", "post_id")).text = str(post["id"])
+        etree.SubElement(item, QName("http://wordpress.org/export/1.2/", "post_id")).text = str(post["post_id"])
         etree.SubElement(item, QName("http://wordpress.org/export/1.2/", "post_date")).text = post["post_date"]
         etree.SubElement(item, QName("http://wordpress.org/export/1.2/", "post_date_gmt")).text = post["post_date_gmt"]
         etree.SubElement(item, QName("http://wordpress.org/export/1.2/", "comment_status")).text = post.get("comment_status", "open")
@@ -39,7 +35,8 @@ def create_wxr(posts, output_path):
         etree.SubElement(item, QName("http://wordpress.org/export/1.2/", "status")).text = post["status"]
         etree.SubElement(item, QName("http://wordpress.org/export/1.2/", "post_type")).text = post.get("post_type", "post")
 
-    # Сохраняем XML в файл
     tree = etree.ElementTree(rss)
-    with open("export.xml", "wb") as f:
+
+    output_path = os.path.join(output_dir, output_file_name)
+    with open(output_path, "wb") as f:
         tree.write(f, encoding="UTF-8", xml_declaration=True, pretty_print=True)
--- main.py
+++ main.py
@@ -3,14 +3,38 @@ from parse_xml import parse_xml
 from prompts import url_translate_prompt, content_translate_prompt
 from dotenv import load_dotenv
 import os
+from pprint import pprint
+
+langs = ['ru', 'es']
+
+
+def gpt_translate(post_data, language):
+    pass
+
+
+def localize_links(post_data, language):
+    pass
 
 
 if __name__ == '__main__':
     load_dotenv('.env')
     api_key = os.environ['GPT_API_KEY']
 
-    post_path = ''
+    post_path = 'getitcom.WordPress.2024-08-01.xml'
     original_post_data = parse_xml(post_path)
+
+    translations = []
+
+    # for lang in langs:
+    #     translated_data = gpt_translate(original_post_data, lang)
+    #     # todo links localization
+    #     # translated_data = localize_links(translated_data, lang)
+    #     translations.append(translated_data)
+
+    translations.append(original_post_data)
+    output_path = 'output_' + post_path
+    # pprint(original_post_data)
+    create_wxr(translations, output_path)
 
     # Пример использования
     posts = [
@@ -30,4 +54,4 @@ if __name__ == '__main__':
         # Добавьте больше постов, если необходимо
     ]
 
-    create_wxr(posts, 'output.xml')
+    # create_wxr(posts, 'output.xml')
