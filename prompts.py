ab55bf192cca395047159023e460064e45e7371c
Emil Shagiev <lesshishkin@gmail.com>
2024-08-04 21:40:19 +0300

    prompts moved to separate file

--- prompts.py
+++ prompts.py
@@ -1,11 +1,29 @@
 url_translate_prompt = """
-
+You are a translator. Translate the following URL slug to {language}, and return the
+translation in Latin characters (transliteration), keeping hyphens between words:
 """
 
 content_translate_prompt = """
-
+You are a translator. Translate the following text to {language} while preserving any 
+HTML tags and other markup exactly as they are in the original text. Do not output anything except the translated text.
+Text:
 """
 
 content_diff_prompt = """
+You are a translation evaluator. Compare the original text and the back-translated text, 
+ignoring any HTML tags and markup. Provide a score from 1 to 5 where: 1 - Extremely poor translation, 2 - Poor 
+translation, 3 - Mediocre translation, 4 - Good translation, 5 - Excellent translation
 
+Additionally, quote the places in the back-translated text and the original text where the meaning has changed or 
+been lost during the translation process.
+
+Original Text:
+{original_text}
+
+Back-translated Text:
+{double_translated_text}
+"""
+
+basic_translate_prompt = """
+You are a translator. Translate the following text to {language}. Do not output anything except the translated text.
 """
--- translation_tools.py
+++ translation_tools.py
@@ -1,18 +1,18 @@
 from gpt_tools import ask_gpt
 from configs import config
 import evaluate
-from prompts import url_translate_prompt, content_translate_prompt, content_diff_prompt
-# todo move prompts to separate file
+from prompts import url_translate_prompt, content_translate_prompt, content_diff_prompt, basic_translate_prompt
+
 
 def translate_post(post_data, language, debug=False):
-    prompt = f"You are a translator. Translate the following text to {config.langs[language]}."
+    prompt = basic_translate_prompt.format(language=config.langs[language])
     text = post_data['title']
     translated_title = ask_gpt(prompt, text)
     if debug:
         print('Title translated')
 
     if post_data['excerpt'] is not None:
-        prompt = f"You are a translator. Translate the following text to {config.langs[language]}."
+        prompt = basic_translate_prompt.format(language=config.langs[language])
         text = post_data['excerpt']
         translated_excerpt = ask_gpt(prompt, text)
     else:
@@ -20,15 +20,13 @@ def translate_post(post_data, language, debug=False):
     if debug:
         print('Excerpt translated')
 
-    prompt = (f"You are a translator. Translate the following text to {config.langs[language]} while preserving any "
-              f"HTML tags and other markup exactly as they are in the original text:")
+    prompt = content_translate_prompt.format(language=config.langs[language])
     text = post_data['content']
     translated_content = ask_gpt(prompt, text)
     if debug:
         print('Content translated')
 
-    prompt = (f"You are a translator. Translate the following URL slug to {config.langs[language]}, and return the "
-              f"translation in Latin characters (transliteration), keeping hyphens between words:")
+    prompt = url_translate_prompt.format(language=config.langs[language])
     text = post_data['link'].split('/')[-1]
     translated_slug = ask_gpt(prompt, text)
 
@@ -41,27 +39,16 @@ def translate_post(post_data, language, debug=False):
 
 def evaluate_translation(translation, original_text):
     # translate the translation back to original lang
-    prompt = (f"You are a translator. Translate the following text to {config.origin_lang} while preserving any "
-              f"HTML tags and other markup exactly as they are in the original text:")
+    prompt = content_translate_prompt.format(language=config.origin_lang)
     double_translated_text = clean_text(ask_gpt(prompt, translation))
     original_text = clean_text(original_text)
+
     # calculate BLEU score
     metric = evaluate.load("bleu")
     bleu_score = metric.compute(double_translated_text, [original_text])
+
     # попросим модель сравнить два текста, дать оценку и процитировать неудачные места перевода
-    prompt = f""" You are a translation evaluator. Compare the original text and the back-translated text, 
-    ignoring any HTML tags and markup. Provide a score from 1 to 5 where: 1 - Extremely poor translation, 2 - Poor 
-    translation, 3 - Mediocre translation, 4 - Good translation, 5 - Excellent translation
-
-    Additionally, quote the places in the back-translated text and the original text where the meaning has changed or 
-    been lost during the translation process.
-
-    Original Text:
-    {original_text}
-
-    Back-translated Text:
-    {double_translated_text}
-    """
+    prompt = content_diff_prompt.format(original_text=original_text, double_translated_text=double_translated_text)
     gpt_score = ask_gpt(prompt)
 
     return bleu_score['bleu'], gpt_score
